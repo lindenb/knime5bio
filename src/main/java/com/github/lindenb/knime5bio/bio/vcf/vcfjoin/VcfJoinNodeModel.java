8308ebca0147862bb3f722e6fd96a60818056295
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-05-19 17:39:57 +0200

    cont

--- src/main/java/com/github/lindenb/knime5bio/bio/vcf/vcfjoin/VcfJoinNodeModel.java
+++ src/main/java/com/github/lindenb/knime5bio/bio/vcf/vcfjoin/VcfJoinNodeModel.java
@@ -1,17 +1,13 @@
 package com.github.lindenb.knime5bio.bio.vcf.vcfjoin;
 import org.knime.core.node.*;
-import org.knime.core.node.defaultnodesettings.SettingsModel;
 import org.knime.core.data.*;
-import org.knime.core.data.container.CloseableRowIterator;
 import org.knime.core.data.def.*;
 
-import com.github.lindenb.jvarkit.tools.vcfcmp.VcfIn;
 import com.github.lindenb.jvarkit.util.htsjdk.HtsjdkVersion;
 import com.github.lindenb.jvarkit.util.picard.SAMSequenceDictionaryProgress;
 import com.github.lindenb.jvarkit.util.vcf.IndexedVcfFileReader;
 import com.github.lindenb.jvarkit.util.vcf.VCFUtils;
 import com.github.lindenb.jvarkit.util.vcf.VcfIterator;
-import com.github.lindenb.knime5bio.AbstractKnime5BioNodeModel;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -32,261 +28,14 @@ import htsjdk.variant.vcf.VCFInfoHeaderLine;
 
 /* read two VCFS, do something , output one vcf */
 public class VcfJoinNodeModel
-	extends AbstractKnime5BioNodeModel
+	extends AbstractVcfJoinNodeModel
 	{	
-	
-	/** VCF path *************************************************************/
 
-
-	final static String CONFIG_VCFINPUT1 = "CONFIG_VCFINPUT1";
-	final static String DEFAULT_VCFINPUT1 = "COLUMN";
-
-	
-	private org.knime.core.node.defaultnodesettings.SettingsModelColumnName m_col1 = 
-		new  org.knime.core.node.defaultnodesettings.SettingsModelColumnName(
-			CONFIG_VCFINPUT1,
-			DEFAULT_VCFINPUT1
-			);
-
-	protected org.knime.core.node.defaultnodesettings.SettingsModelColumnName getPropertyVcfInput1Settings()
-		{
-		return this.m_col1;
-		}
-	
-	protected String getPropertyVcfInput1Value()
-		{
-		return getPropertyVcfInput1Settings().getStringValue();
-		}
-
-	protected int findVcfInput1ColumnIndex(final  DataTableSpec inSpec)
-		{
-		int index= inSpec.findColumnIndex(getPropertyVcfInput1Value());
-		
-		return index;
-		}
-	
-	protected int findVcfInput1RequiredColumnIndex(final  DataTableSpec inSpec)
-		throws InvalidSettingsException
-		{
-		int index =  findVcfInput1ColumnIndex(inSpec);
-		if(index == -1 )
-			{
-			throw new InvalidSettingsException("Node "+this.getClass().getName()+": cannot find column title= \"VCF path\"");
-			}
-
-		return index;
-		}
-
-	
-	/** VCF path *************************************************************/
-
-
-	final static String CONFIG_VCFINPUT2 = "CONFIG_VCFINPUT2";
-	final static String DEFAULT_VCFINPUT2 = "COLUMN";
-
-	
-	private org.knime.core.node.defaultnodesettings.SettingsModelColumnName m_col2 = 
-		new  org.knime.core.node.defaultnodesettings.SettingsModelColumnName(
-			CONFIG_VCFINPUT2,
-			DEFAULT_VCFINPUT2
-			);
-
-	protected org.knime.core.node.defaultnodesettings.SettingsModelColumnName getPropertyVcfInput2Settings()
-		{
-		return this.m_col2;
-		}
-	
-	protected String getPropertyVcfInput2Value()
-		{
-		return getPropertyVcfInput2Settings().getStringValue();
-		}
-
-	protected int findVcfInput2ColumnIndex(final  DataTableSpec inSpec)
-		{
-		int index= inSpec.findColumnIndex(getPropertyVcfInput2Value());
-		
-		return index;
-		}
-	
-	protected int findVcfInput2RequiredColumnIndex(final  DataTableSpec inSpec)
-		throws InvalidSettingsException
-		{
-		int index =  findVcfInput2ColumnIndex(inSpec);
-		if(index == -1 )
-			{
-			throw new InvalidSettingsException("Node "+this.getClass().getName()+": cannot find column title= \"VCF path\"");
-			}
-
-		return index;
-		}
-	/* inverse property */
-	final static String CONFIG_INVERSE = "CONFIG_INVERSE";
-	final static boolean DEFAULT_INVERSE = false;
-
-	private org.knime.core.node.defaultnodesettings.SettingsModelBoolean m_inverse = 
-		new  org.knime.core.node.defaultnodesettings.SettingsModelBoolean(
-			CONFIG_INVERSE,
-			DEFAULT_INVERSE
-			);
-		
-	protected org.knime.core.node.defaultnodesettings.SettingsModelBoolean getPropertyInverseSettings()
-		{
-		return this.m_inverse;
-		}
-	
-	protected boolean isPropertyInverseValue()
-		{
-		return getPropertyInverseSettings().getBooleanValue();
-		}
-
-	/* alt match */
-	final static String CONFIG_ALT_IN_DB = "ALT_IN_DB";
-	final static boolean DEFAULT_ALT_IN_DB = false;
-
-	private org.knime.core.node.defaultnodesettings.SettingsModelBoolean m_altInIndex = 
-		new  org.knime.core.node.defaultnodesettings.SettingsModelBoolean(
-			CONFIG_ALT_IN_DB,
-			DEFAULT_ALT_IN_DB
-			);
-		
-	protected org.knime.core.node.defaultnodesettings.SettingsModelBoolean getPropertyAltInIndexSettings()
-		{
-		return this.m_altInIndex;
-		}
-	
-	protected boolean isPropertyAltInIndexValue()
-		{
-		return getPropertyAltInIndexSettings().getBooleanValue();
-		}
-	
-	
-	/* Peek Info Fields */
-	final static String CONFIG_PEEK_INFO = "PEEK_INFO";
-	final static String DEFAULT_PEEK_INFO = "";
-
-	private org.knime.core.node.defaultnodesettings.SettingsModelString m_peekInfo = 
-		new  org.knime.core.node.defaultnodesettings.SettingsModelString(
-				CONFIG_PEEK_INFO,
-				DEFAULT_PEEK_INFO
-			);
-		
-	protected org.knime.core.node.defaultnodesettings.SettingsModelString getPropertyPeekInfoSettings()
-		{
-		return this.m_peekInfo;
-		}
-	
-	protected String getPropertyPeekInfoValue()
-		{
-		return getPropertyPeekInfoSettings().getStringValue();
-		}
-	
-
-	
-	
-	@Override
-	protected String getSettingsModelSummary()
-		{
-		StringBuilder b=new StringBuilder();
-		
-		b.append("vcf1").append("=").append(String.valueOf(getPropertyVcfInput1Value()));
-		b.append(" vcf2").append("=").append(String.valueOf(getPropertyVcfInput2Value()));
-		b.append(" inverse").append("=").append(String.valueOf(isPropertyInverseValue()));
-		b.append(" altInIndex").append("=").append(String.valueOf(isPropertyAltInIndexValue()));
-		b.append(" peekinfo").append("=").append(String.valueOf(getPropertyPeekInfoValue()));
-		
-		return b.toString();
-		}
-
-	
 	public VcfJoinNodeModel()
 		{
 		/* super(inport,outport) */
-		super(2,1);
+		super();
 		}
-	
-	
-	@Override 
-	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs) throws InvalidSettingsException
-		{
-    	if(inSpecs==null || inSpecs.length!= 1)
-			{
-			throw new InvalidSettingsException("Expected  1 tables");
-			}
-    	DataTableSpec datatablespecs[] = new DataTableSpec[1];
-    	
-    	datatablespecs[0] = 
-    		configureOutDataTableSpec(0,inSpecs);
-    		
-    	return datatablespecs;
-    	}
-
-	
-	/* @inheritDoc */
-	@Override
-    protected DataTableSpec createOutDataTableSpec(int index)
-    	{
-    	switch(index)
-    		{
-    		case 0 : return createOutTableSpec0();
-    		
-    		default: throw new IllegalStateException();
-    		}
-    	}
-    /** configure output port for known-index for known inSpecs */	
-	protected DataTableSpec configureOutDataTableSpec(int index,DataTableSpec[] inSpecs) throws InvalidSettingsException
-		{
-		switch(index)
-    		{
-    		case 0 : return configureOutTableSpec0(inSpecs);
-    		
-    		default: throw new IllegalStateException();
-    		}
-		}
-	
-	protected DataCell[] createDataCellsForOutTableSpec0(
-			CharSequence  vcfOut,java.lang.Integer  countVariants
-			)
-			{
-			DataCell __cells[]=new DataCell[2];
-			__cells[0 ] = ( vcfOut !=null? new StringCell( vcfOut.toString() ) :  DataType.getMissingCell() ) ;
-			__cells[1 ] =  ( countVariants !=null? new IntCell( countVariants ) :  DataType.getMissingCell() );
-			
-			return __cells;
-			}
-		
-		
-		/** create DataTableSpec for outport '0' outpout */
-		protected DataTableSpec createOutTableSpec0()
-			{
-			
-			DataColumnSpec colspecs[]=new DataColumnSpec[ 2 ];
-			
-			colspecs[ 0 ] = new org.knime.core.data.DataColumnSpecCreator("vcfOut",org.knime.core.data.DataType.getType(org.knime.core.data.def.StringCell.class)).createSpec();
-			
-			colspecs[ 1 ] = new org.knime.core.data.DataColumnSpecCreator("countVariants",org.knime.core.data.DataType.getType(org.knime.core.data.def.IntCell.class)).createSpec();
-			
-	    	return new DataTableSpec(colspecs);
-			
-			
-			}
-		/** create DataTableSpec for outport '0' outpout for known DataTableSpec */
-		protected DataTableSpec configureOutTableSpec0(DataTableSpec[] inSpecs) throws InvalidSettingsException
-			{
-			return this.createOutTableSpec0();
-			}	
-		
-		/* @inheritDoc */
-		@Override
-	    protected java.util.List<SettingsModel> fillSettingsModel(java.util.List<SettingsModel> list) {
-	    	list = super.fillSettingsModel(list);
-	    	
-	    	list.add(this.getPropertyVcfInput1Settings());
-	    	list.add(this.getPropertyVcfInput2Settings());
-	    	list.add(this.getPropertyInverseSettings());
-	    	list.add(this.getPropertyAltInIndexSettings());
-	    	list.add(this.getPropertyPeekInfoSettings());
-	    	return list;
-	    	}
 
 	
 	
@@ -347,7 +96,7 @@ public class VcfJoinNodeModel
 		            	}	
 		            resource2=uri;
 	                }
-		     CloserUtil.close( iter);
+		     CloserUtil.close( iter);iter=null;
 		     if(resource2==null)
 		     	{
 	            throw new RuntimeException("port n°2: input resource not found");
@@ -389,8 +138,10 @@ public class VcfJoinNodeModel
 	                	{
 	                	fileout.getParentFile().mkdirs();
 	                	}
+	                /* open streams */
 	                in = VCFUtils.createVcfIterator(uri);
 	                w = VCFUtils.createVariantContextWriter(fileout);
+	                /* create header */
 	                VCFHeader header=in.getHeader();
 	                VCFHeader h2=new VCFHeader(header);
             		h2.addMetaDataLine(new VCFHeaderLine(getClass().getSimpleName()+"HtsJdkVersion",HtsjdkVersion.getVersion()));
@@ -409,6 +160,7 @@ public class VcfJoinNodeModel
 	                SAMSequenceDictionaryProgress progress=new SAMSequenceDictionaryProgress(header);
 	                w.writeHeader(h2);
 	                int count=0;
+	                /* loop over variants */
 	                while(in.hasNext())
 	                	{
 	                	VariantContext ctx = progress.watch(in.next());
@@ -462,8 +214,8 @@ public class VcfJoinNodeModel
 	                		++count;
 	                		}
 	                	}
-	                w.close();
-	                in.close();
+	                w.close();w=null;
+	                in.close();in=null;
 	                
 					out_container.addRowToTable(new DefaultRow(
 							RowKey.createRowKey(nRows),
@@ -494,7 +246,6 @@ public class VcfJoinNodeModel
 	            }
 	        }
 
-		/** END user code/body */
 
 	
 	}
